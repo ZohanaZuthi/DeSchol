cb1e423b143af0bd25a2bf178ce55586ac0ef25a
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2025-07-04 19:01:34 +0600

    Initial Commit

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.js";
 import cookieParser from "cookie-parser";
+import connectDB from "./utils/db.js";
 import cors from "cors";
 
+
+dotenv.config();
 const app = express();
 
 app.use(express.json());
@@ -22,19 +25,13 @@ app.use(cors(corsOptions)); // add CORS middleware
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running on port ${PORT}`);
 });
 
 
 
-app.get("/", (req, res) => {
-    return res.status(200).json(
-        {
-            message: "I am coming from backend",
-            success: true
-        }
-    )
-  });
+
 
 
   
---
+++ models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Student", "Recruiter"],
+      required: true,
+    },
+  
+    profile: {
+      image: {
+        type: String,
+        default: "https://cdn-icons-png.flaticon.com/128/149/149071.png",
+      },
+      bio: String,
+  
+      // Only for Students
+      department: String,
+      CG: Number,
+      skills: [String],
+      extracurricularActivities: String,
+      resume: String,
+      linkedin: String,
+      github: String,
+      portfolio: String,
+  
+      // Only for Recruiters
+      institution: String,
+      position: String,
+      institutionLocation: String,
+    },
+  },{timestamps:true});
+//   for adding the updated time and created time automatically
+
+  export const User=mongoose.model("User",userSchema);
+
+
+  
