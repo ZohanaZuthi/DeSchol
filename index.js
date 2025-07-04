6065e03b834ca6a78d8fa6716790c699a8e1cba9
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2025-07-04 14:27:56 +0600

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Backend is live 🎉");
+  });
+  
   
